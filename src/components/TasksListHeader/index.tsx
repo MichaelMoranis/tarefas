b2e7455dd3ac6f5a70d27f530cb0b7d99c6c2730
michaelmoranis <michaelmoranesul@gmail.com>
2024-09-22 11:32:01 -0300

    fix: configurando git no terminal

--- src/components/TasksListHeader/index.tsx
+++ src/components/TasksListHeader/index.tsx
@@ -4,7 +4,7 @@ interface taskCompletedProps {
 
 export function TaskListHeader({ taskCompleted }: taskCompletedProps) {
   return (
-    <div className="flex gap-2 text-xl  justify-center text-zinc-700 px-2 ml-2 bg-orange-400 rounded-md w-48">
+    <div className="flex gap-2 text-xl  justify-center font-bold text-zinc-800 px-2 ml-2 bg-orange-400 rounded-md w-48">
       concluidas:
       <span>{taskCompleted}</span>
     </div>
